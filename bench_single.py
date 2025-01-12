a7844d1c19eb57cffad81d7063124d6726e6f057
Krzysztof Majerczyk-Marchewka <skanerooo5@gmail.com>
2025-01-12 18:02:18 +0100

    Fixed base64 decoder to use STD encoding instead of URL endoding

--- bench_single.py
+++ bench_single.py
@@ -2,14 +2,14 @@ from requests import get
 
 
 def get_render_time(hash):
-    url = f'http://127.0.0.1:3000/render/card/{hash}'
-    return float(get(url).headers['X-Processing-Time'])
+    url = f'http://127.0.0.1:8899/render/card/{hash}'
+    return float(get(url).headers['X-Processing-Time'][:-2])
 
 
 def main():
     average = 0
     for _ in range(1000):
-        average += get_render_time("eyJpZCI6MTk1NCwiZnJhbWUiOjIsImdsb3ciOnRydWUsImR5ZSI6MjYxNjU4MH0")
+        average += get_render_time("eyJpZCI6MTk1NCwiZnJhbWUiOjIsImdsb3ciOnRydWUsImR5ZSI6MjYxNjU4MH0=")
     print(average / 1000)
 
 
