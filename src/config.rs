c2e134f8b62338795eaa28b6757c8e25586b6552
amatsagu <krzysztof.rapior@protonmail.com>
2025-06-12 02:14:06 +0200

    feat: update server port

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, sync::LazyLock};
 
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
-pub const ADDRESS: &str = "127.0.0.1:8080";
+pub const ADDRESS: &str = "127.0.0.1:8899";
 pub const AUTH_TOKEN: &str = "a1fe0d2d2469bb472016d667be975b51";
 
 pub const CDN_FRAMES_PATH: &str = "../asset/private/frame";
@@ -11,11 +11,6 @@ pub const CDN_CHARACTER_IMAGES_PATH: &str = "../asset/private/character";
 pub const CDN_CARD_IMAGES_PATH: &str = "../asset/private/custom-character-card";
 pub const CDN_RENDERS_PATH: &str = "../asset/public/render";
 
-pub const CHARACTER_IMAGE_X: u32 = 245;     // 💀 NEVER USED
-pub const CHARACTER_IMAGE_Y: u32 = 370;     // 💀 NEVER USED
-pub const CARD_MAX_X: u32 = 303;            // 💀 NEVER USED
-pub const CARD_MAX_Y: u32 = 428;            // 💀 NEVER USED
-pub const DEFAULT_DYE_COLOR: u32 = 8289918; // 💀 NEVER USED
 pub const RENDER_TIMEOUT: f32 = 10.0; // in seconds
 
 pub const FAN_CARD_ANGLE: f32 = 5.0;
