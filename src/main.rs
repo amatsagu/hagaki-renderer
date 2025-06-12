ed13629f34e550a3a34340ec34a11962d114e7ef
amatsagu <krzysztof.rapior@protonmail.com>
2025-06-12 02:14:33 +0200

    feat: add basic logging

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@ mod models;
 mod utils;
 
 use std::sync::Arc;
-
-use axum::{body::Body, http::Response, routing::{delete, get}, serve, Extension, Router};
+use axum::{body::Body, http::Response, routing::{get}, serve, Extension, Router};
 use tokio::net::TcpListener;
+use log::{info, error};
+use pretty_env_logger::init as init_logger;
+
 use crate::handlers::render::{handle_card_album_request, handle_card_request, handle_card_fan_request};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() {
+    init_logger();
+    info!("Starting service...");
 
     let frames = Arc::new(utils::load_frames());
 
@@ -26,8 +30,20 @@ async fn main() -> Result<(), std::io::Error> {
         .fallback(|| async { Response::builder().status(418).body(Body::empty()).unwrap() })
         .layer(Extension(frames));
 
-    let listener = TcpListener::bind(config::ADDRESS).await.unwrap();
+    match TcpListener::bind(config::ADDRESS).await {
+        Ok(listener) => {
+            info!("Starting http server at {}. Server is ready.", config::ADDRESS);
+            if let Err(e) = serve(listener, router).await {
+                error!("Server encountered an error: {}", e);
+                std::process::exit(1);
+            }
+        }
+        Err(err) => {
+            error!("Failed to bind to {} tcp socket: {}", config::ADDRESS, err);
+            std::process::exit(1);
+        }
+    }
 
-    serve(listener, router).await
+    info!("Successfully closed http server. Bye!")
 }
 
