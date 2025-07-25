32b63c5844a8b5f88935f75bc30e8c7fcf3d4159
amatsagu <krzysztof.rapior@protonmail.com>
2025-07-25 21:22:52 +0200

    feat: added graceful shutdown handler

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-mod handlers;
 mod config;
+mod handlers;
 mod models;
 mod utils;
 
-use std::sync::Arc;
-use axum::{body::Body, http::Response, routing::{get}, serve, Extension, Router};
-use tokio::net::TcpListener;
-use log::{info, error};
+use axum::{body::Body, http::Response, routing::get, serve, Extension, Router};
+use log::{error, info};
 use pretty_env_logger::init as init_logger;
+use std::sync::Arc;
+use tokio::net::TcpListener;
+use tokio::signal;
 
-use crate::handlers::render::{handle_card_album_request, handle_card_request, handle_card_fan_request};
+use crate::handlers::render::{
+    handle_card_album_request, handle_card_fan_request, handle_card_request,
+};
 
 #[tokio::main]
 async fn main() {
@@ -24,16 +27,18 @@ async fn main() {
             Router::new()
                 .route("/card/{hash}", get(handle_card_request))
                 .route("/fan/{hash}", get(handle_card_fan_request))
-                .route("/album/{hash}", get(handle_card_album_request))
-                //.route("/{file_name}", delete(handlers::render_remove))
+                .route("/album/{hash}", get(handle_card_album_request)), //.route("/{file_name}", delete(handlers::render_remove))
         )
         .fallback(|| async { Response::builder().status(418).body(Body::empty()).unwrap() })
         .layer(Extension(frames));
 
     match TcpListener::bind(config::ADDRESS).await {
         Ok(listener) => {
-            info!("Starting http server at {}. Server is ready.", config::ADDRESS);
-            if let Err(e) = serve(listener, router).await {
+            info!(
+                "Starting http server at {}. Server is ready.",
+                config::ADDRESS
+            );
+            if let Err(e) = serve(listener, router).with_graceful_shutdown(shutdown_signal()).await {
                 error!("Server encountered an error: {}", e);
                 std::process::exit(1);
             }
@@ -47,3 +52,29 @@ async fn main() {
     info!("Successfully closed http server. Bye!")
 }
 
+// https://github.com/tokio-rs/axum/blob/main/examples/graceful-shutdown/src/main.rs
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    info!("Received signal to shutdown. Starting graceful shutdown!");
+}
